64ad3bfd3c6e34ec43b04803df5b4d0a3030d3ba
Eric Berquist <eric.berquist@gmail.com>
2023-02-15 14:30:49 -0500

    pre-commit

--- src/scikit_build_example/__init__.py
+++ src/scikit_build_example/__init__.py
@@ -1,3 +1,3 @@
-from ._core import __doc__, __version__, add, subtract, Pet, Dog
+from ._core import Dog, Pet, __doc__, __version__, add, subtract
 
 __all__ = ["__doc__", "__version__", "add", "subtract", Pet, Dog]
