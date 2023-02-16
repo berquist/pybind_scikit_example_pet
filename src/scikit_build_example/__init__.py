38c9bacfba34a211a3f1c97624eee93b86b28197
Eric Berquist <eric.berquist@gmail.com>
2023-02-16 10:59:29 -0500

    fix __all__

--- src/scikit_build_example/__init__.py
+++ src/scikit_build_example/__init__.py
@@ -1,3 +1,3 @@
 from ._core import Dog, Pet, __doc__, __version__, add, subtract
 
-__all__ = ["__doc__", "__version__", "add", "subtract", Pet, Dog]
+__all__ = ["__doc__", "__version__", "add", "subtract", "Pet", "Dog"]
